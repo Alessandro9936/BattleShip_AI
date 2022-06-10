ebf75b7cc30d45a9975ca38156e6cb85652cd5f6
Alessandro Crinella <alecrinella99@gmail.com>
2022-06-10 11:48:53 +0200

    Add type of ships in helper folder
    The type of ship is used to retrieve the ship that is gonna be placed
    on the gameBoard

---
+++ src/helpers/typeOfShips.js
@@ -0,0 +1,8 @@
+const typeOfShips = [
+  { id: "carrier", length: 5 },
+  { id: "battleship", length: 4 },
+  { id: "cruiser", length: 3 },
+  { id: "submarine", length: 3 },
+  { id: "destroyer", length: 2 },
+];
+export default typeOfShips;
