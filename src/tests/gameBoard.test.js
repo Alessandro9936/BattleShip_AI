e266607cfb6f3e97a7b8e91a09d1493f9fa2c94f
Alessandro Crinella <alecrinella99@gmail.com>
2022-06-12 11:26:13 +0200

    Add gameBoardFactory receiveAttack tests

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -6,6 +6,7 @@ jest.mock("../factories/ship");
 
 let testBoard;
 beforeEach(() => (testBoard = new gameBoard()));
+
 it("create array when instance of gameBoard is created", () => {
   expect(testBoard.board).toHaveLength(100);
 });
@@ -45,5 +46,35 @@ describe("placeShip()", () => {
     expect(testBoard.board[3].hasShip).not.toBeFalsy();
     expect(testBoard.board[4].hasShip).not.toBeFalsy();
     expect(testBoard.board[5].hasShip).toBeFalsy();
+  });
+});
+describe("receiveAttack ()", () => {
+  it("check coord in gameBoard to hit if coord is given", () => {
+    const entry = 2;
+    testBoard.placeShip(entry, typeOfShips[2]);
+    const attackedCoord = 2;
+    testBoard.receiveAttack(attackedCoord);
+
+    expect(testBoard.board[attackedCoord].isHit).toBe(true);
+  });
+
+  it("attacked cell has a ship, expect to call hit method on the ship placed at attacked cell", () => {
+    const entry = 2;
+    testBoard.placeShip(entry, typeOfShips[2]);
+    const attackedCoord = 2;
+    testBoard.receiveAttack(attackedCoord);
+
+    expect(testBoard.board[attackedCoord].hasShip.hit).toHaveBeenCalledTimes(1);
+  });
+
+  it("attacked cell has a ship, expect to call isSunk method on the ship placed at attacked cell", () => {
+    const entry = 2;
+    testBoard.placeShip(entry, typeOfShips[2]);
+    const attackedCoord = 2;
+    testBoard.receiveAttack(attackedCoord);
+
+    expect(testBoard.board[attackedCoord].hasShip.isSunk).toHaveBeenCalledTimes(
+      1
+    );
   });
 });
